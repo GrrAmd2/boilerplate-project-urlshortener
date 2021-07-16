8d5a7917d79e4518e29e234f47abdab706db9dde
Perez Alan <diegoalanperezz@gmail.com>
2021-07-15 23:04:13 -0300

    all done

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require("mongoose");
+const { isValid, UrlShort } = require('./Shortener');
+const { nanoid } = require('nanoid');
+
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+app.use(express.urlencoded({extended: false}))
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -15,9 +20,27 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', async (req, res) => {
+  const {url} = req.body;
+  if(isValid(url)){
+
+      const urlObj = await UrlShort.create({
+        original_url: url,
+        short_url: nanoid(5)
+      });
+      return res.json(urlObj)
+  }
+  return res.json({error: 'invalid url'})
 });
+
+app.get('/api/shorturl/:short', async(req, res) => {
+  const short = req.params.short;
+  const exist = await UrlShort.findOne({short_url: short}).exec();
+  if(exist){
+    return res.redirect(exist.original_url)
+  }
+  return res.json({error: 'invalid url'})
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
